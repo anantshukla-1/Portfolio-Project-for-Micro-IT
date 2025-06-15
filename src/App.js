40ba5d06be1fb07e9ae7bda2dd9efa16ec01240f
anantshukla-1 <shukla18anant9839@gmail.com>
2025-06-15 22:43:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import LandingPage from './components/LandingPage';
+import Sidebar from './components/Sidebar';
+import StickyBanner from './components/StickyBanner';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <div className="relative min-h-screen bg-gray-50 transition-all duration-500">
+
+      <Sidebar />
+      <StickyBanner />
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
